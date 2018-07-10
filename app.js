ae8ec5310097f9e3fe9ef9c1f44a65f89e80cab4
Daron McIntosh <mcintoshdaron@gmail.com>
2018-07-10 02:25:36 -0400

    replace html5 player with plyr, add header and footer for all html docs, add bootstrap and update api response

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const express = require("express");
 const ytdl = require("ytdl-core");
 const bodyParser = require("body-parser");
 const stream = require("youtube-audio-stream");
+const ypi = require("youtube-playlist-info");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public")); // eslint-disable-line
+
+// ypi("AIzaSyA3dRQiPt2aHwZ3piY_nJ7tZH6XEEDYTMM", "PLBCF2DAC6FFB574DE").then(items => {
+// 	console.log(items);
+// }).catch(console.error);
 
 // INDEX PAGE
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 	res.render("index");
 });
 
-// SOURCE URL FOR AUDIOn
-app.get("/api/play/:videoId", function(req, res){
+// SOURCE URL FOR AUDIO
+app.get("/api/play/:videoId", function (req, res) {
 	// Secure this route to prevent unauthorized access and/or convert to a post route
 	// Find a better name for this route
 	var requestUrl = "http://youtube.com/watch?v=" + req.params.videoId;
@@ -27,40 +32,51 @@ app.get("/api/play/:videoId", function(req, res){
 });
 
 // API RESPONSE ROUTE
-app.get("/api/request/", function(req, res){
+app.get("/api/request/", function (req, res) {
 	// SEND JSON DATA TO THIS ROUTE
 	// Verify that the url is valid and contains a valid id to prevent errors
-	var videoId = ytdl.getVideoID(req.query.url);
-	var jsonResponse = {
-		id: videoId,
-		info: "This is an API response in JSON format",
-		data: {
-			url: "http://localhost:3000/player/" + videoId
+	var videoId = ytdl.getVideoID(req.query.apiURL);
+	ytdl.getInfo(req.query.apiURL, function (err, info) {
+		if (err) {
+			res.redirect("back");
 		}
-	};
-	res.type("json");
-	res.write(JSON.stringify(jsonResponse));
-	res.end();
+		var length = info.length_seconds;
+		var title = info.title;
+		var jsonResponse = {
+			videoId: videoId,
+			title: title,
+			length: length + " seconds",
+			data: {
+				url: "http://localhost:3000/api/play/" + videoId
+			}
+		};
+		res.type("json");
+		res.write(JSON.stringify(jsonResponse));
+		res.end();
+	});
 });
 
 // Basic HTLM5 Player
-app.get("/player/:source", function(req, res){
+app.get("/player/:source", function (req, res) {
 	// This route should only play streams from this domain
-	var source = req.params.source;
-	res.render("player", {source: source});
+	ytdl.getInfo(req.params.source, function (err, info) {
+		var source = req.params.source;
+		var length = info.length_seconds;
+		res.render("player", { source: source, length: length });
+	});
 });
 
 // Redirection route to get to player from index page
-app.get("/redirection", function(req, res){
-	var videoId = ytdl.getVideoID(req.query.url);
+app.get("/redirection", function (req, res) {
+	var videoId = ytdl.getVideoID(req.query.playURL);
 	res.redirect("/player/" + videoId);
 });
 
-app.get("*", function(req, res){
+app.get("*", function (req, res) {
 	res.render("404");
 });
 
 // Listen on port 3000
-app.listen(3000, function(){
+app.listen(3000, function () {
 	console.log("Server has started on port 3000");
 });
