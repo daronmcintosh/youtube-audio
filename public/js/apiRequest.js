fafca03dcde9e13a5487a6c91a88e62521358d92
Daron McIntosh <mcintoshdaron@gmail.com>
2018-07-15 14:10:09 -0400

    make player page look similar to playlist page for consistency

--- public/js/apiRequest.js
+++ public/js/apiRequest.js
@@ -10,14 +10,14 @@ const youtube = google.youtube({
 });
 
 async function buildPlaylist(playlistId) {
-	const res = await youtube.playlistItems.list({
+	const result = await youtube.playlistItems.list({
 		playlistId: playlistId,
 		maxResults: 25,
 		part: "snippet"
 	});
 	var tracks = [];
 	var trackCounter = 1;
-	var items = res.data.items;
+	var items = result.data.items;
 	for (const item of items) {
 		var trackObj = {};
 		var videoId = item.snippet.resourceId.videoId;
@@ -32,12 +32,12 @@ async function buildPlaylist(playlistId) {
 	return tracks;
 }
 
-async function buildVideo(videoId){
+async function buildVideo(videoId) {
 	let videoObj = {};
 	await youtube.videos.list({
 		id: videoId,
 		part: "contentDetails, snippet"
-	}).then(function(result){
+	}).then(function (result) {
 		videoObj.title = result.data.items[0].snippet.title;
 		videoObj.duration = moment.duration(result.data.items[0].contentDetails.duration).asSeconds();
 		videoObj.src = "http://localhost:3000/api/play/" + videoId;
@@ -45,5 +45,37 @@ async function buildVideo(videoId){
 	return videoObj;
 }
 
+async function buildSearch(query) {
+	const result = await youtube.search.list({
+		type: "",
+		q: query,
+		maxResults: 25,
+		part: "snippet"
+	});
+	let searchResults = [];
+	var items = result.data.items;
+	for (const item of items) {
+		var searchObj = {};
+		var kind = item.kind;
+		searchObj.kind = kind;
+		if (kind === "youtube#video") {
+			searchObj.videoId = item.id.videoId;
+		}
+		if (kind === "youtube#playlist") {
+			searchObj.playlistId = item.id.playlistId;
+		}
+		searchObj.title = item.snippet.title;
+		searchObj.channelTitle = item.snippet.channelTitle;
+
+		// build playlist or video object around which item was clicked instead of making another api request here
+		// let videoObj = await buildVideo(videoId);
+		// searchObj.duration = await videoObj.duration;
+		// searchObj.src = "http://localhost:3000/api/play/" + videoId;
+		searchResults.push(searchObj);
+	}
+	return searchResults;
+}
+
 module.exports.buildPlaylist = buildPlaylist;
 module.exports.buildVideo = buildVideo;
+module.exports.buildSearch = buildSearch;
