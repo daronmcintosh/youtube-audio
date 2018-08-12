35b83482a3fde8c141fc5a2b14d049217f12c188
Daron McIntosh <mcintoshdaron@gmail.com>
2018-08-12 19:55:20 -0400

    modify logging

--- app.js
+++ app.js
@@ -20,16 +20,9 @@ const { createLogger, format, transports } = require('winston');
 const logger = createLogger({
 	level: 'info',
 	format: format.combine(format.timestamp(), format.json()),
-	transports: [
-		new transports.File({ filename: './logs/error.log', level: 'error' }),
-		new transports.File({ filename: './logs/info.log', level: 'info' })
-	]
+	transports: new transports.Console()
 });
-if (process.env.NODE_ENV !== 'production') {
-	logger.add(new transports.Console({
-		format: format.simple()
-	}));
-}
+
 app.use(session);
 io.use(sharedsession(session));
 
@@ -60,7 +53,7 @@ io.on('connection', (socket) => {
 // INDEX PAGE
 app.get('/', (req, res) => {
 	apiRequest.buildTrendingVideos().then((videos) => {
-		res.render('index', { videos: videos});
+		res.render('index', { videos: videos });
 	}).catch((err) => {
 		if (err) {
 			logger.error(`Index Page: ${err}`);
